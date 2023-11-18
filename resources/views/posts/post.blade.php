69f5face3f9791407cc30eb53309ec2c1d258b4f
nekkox <klascibox@gmail.com>
2023-11-18 18:37:00 +0100

    changing into laravel components

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,15 +1,15 @@
-@extends('layouts.app')
+<x-layout>
 
-@section('content')
-  <article class="container p-5 lh-base fst-normal fs-5 " style="width: 50rem;">
+    <article class="container p-5 lh-base fst-normal fs-5 " style="width: 50rem;">
 
-      <h1>{{$post->title}}</h1>
-      <p>By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> </p>
-      <p>{{$post->body }}</p>
+        <h1>{{$post->title}}</h1>
+        <p>By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
+                href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>{{$post->body }}</p>
 
 
-      <a class="btn btn-outline-danger p-2 shadow-sm " href="{{ url('posts') }}">Back to All Posts</a>
+        <a class="btn btn-outline-danger p-2 shadow-sm " href="{{ url('posts') }}">Back to All Posts</a>
 
-  </article>
+    </article>
 
-@endsection
+</x-layout>
--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -1,34 +1,39 @@
-@extends('layouts.app')
+<x-layout>
 
-@section('content')
-    <div class="container-fluid " style=" background: radial-gradient(circle, rgba(169,16,121,1) 0%, rgba(64,4,80,1) 61%, rgba(46,2,73,1) 100%) no-repeat;  ">
-        <div class="d-flex  flex-column align-items-center" style="height: 100vh">
+        <div class="container-fluid "
+             style=" background: radial-gradient(circle, rgba(169,16,121,1) 0%, rgba(64,4,80,1) 61%, rgba(46,2,73,1) 100%) no-repeat;  ">
+            <div class="d-flex  flex-column align-items-center" style="height: 100vh">
 
 
+                @foreach($posts as $post)
+                    <article>
+                        <div class="card my-5 shadow-lg border-secondary border-opacity-50"
+                             style="width: 50rem; min-height: 15rem; max-height: 20rem">
+                            <div class="card-header text-center bg-dark-subtle fw-bold ">
+                                <a href="{{ url('posts/'.$post->slug) }}"> {{$post->title}} </a>
+                                <p class="text-start">By <a
+                                        href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
+                                        href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
-            @foreach($posts as $post)
-                <article>
-                    <div class="card my-5 shadow-lg border-secondary border-opacity-50" style="width: 50rem; min-height: 15rem; max-height: 20rem">
-                        <div class="card-header text-center bg-dark-subtle fw-bold ">
-                            <a href="{{ url('posts/'.$post->slug) }}"> {{$post->title}} </a>
-                            <p class="text-start">By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> </p>
-
+                            </div>
+                            <div class="card-body text-center overflow-hidden inner " style="height: auto;">
+                                <p class=""> {{ $post->excerpt }} </p>
+                            </div>
+                            <div class="card-footer">
+                                Category:
+                                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                            </div>
                         </div>
-                        <div class="card-body text-center overflow-hidden inner " style="height: auto;">
-                        <p class=""> {{ $post->excerpt }} </p>
-                        </div>
-                        <div class="card-footer">
-                           Category:
-                            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-                        </div>
-                    </div>
-                </article>
+                    </article>
 
-            @endforeach
+                @endforeach
 
+            </div>
         </div>
-    </div>
-@endsection
+
+
+
+</x-layout>
 
 
 
