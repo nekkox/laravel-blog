6199dba3a8e4488806e0da05ed816fbd9c207781
nekkox <klascibox@gmail.com>
2023-11-21 23:59:53 +0100

    Creating dropdown comonent step forward

--- resources/views/_post-header.blade.php
+++ resources/views/_post-header.blade.php
@@ -15,8 +15,25 @@
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
 
-            <x-dropdown :categories="$categories">
+            <x-dropdown>
 
+                {{-- Defining trigger slot --}}
+                <x-slot name="trigger">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left inline-flex items-center">
+                        {{ isset($currentCategory) ? $currentCategory->name : 'Category' }}
+
+                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                             height="22" viewBox="0 0 22 22">
+                            <g fill="none" fill-rule="evenodd">
+                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z"></path>
+                                <path fill="#222"
+                                      d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                            </g>
+                        </svg>
+                    </button>
+                </x-slot>
+
+                    {{--All goes into $Slot in dropdown component --}}
                 <a href="/posts/"
                    class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">All</a>
                 @foreach($categories as $category)
