4301c3f6be5c2ef537f4a4a696548e8a530567af
nekkox <klascibox@gmail.com>
2024-06-16 11:02:03 +0200

    Create Comment section

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -58,13 +58,29 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+
+                    <form class="border border-gray-200 p-6 rounded-xl" method="post" action="#">
+                        @csrf
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40"
+                                 class="rounded-full">
+                            <h2 class="ml-4">Add Something</h2>
+                        </header>
+                        <div class="mt-8">
+                            <textarea class="w-full text-sm focus:outline-none focus:ring" name="body" cols="50"
+                                      rows="5" placeholder="Say Something"></textarea>
+                        </div>
+                        <div class="flex justify-end mt-6 pt-6 border-t border-gray-200 ">
+                            <button type="submit"
+                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
+                                Post
+                            </button>
+                        </div>
+                    </form>
+
                     @foreach($post->comments as $comment)
-
-                        <x-post-comment :comment="$comment" />
-
+                        <x-post-comment :comment="$comment"/>
                     @endforeach
-
-
                 </section>
             </article>
         </main>
